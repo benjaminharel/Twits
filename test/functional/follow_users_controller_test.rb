b5f7abc06cfe3691b1aabeed0e2cba342575c7d8
benjaminharel <benjamin@skanax.com>
2013-03-18 22:58:54 +0200

    second1

--- test/functional/follow_users_controller_test.rb
+++ test/functional/follow_users_controller_test.rb
@@ -5,7 +5,7 @@ class FollowUsersControllerTest < ActionController::TestCase
     @follow_user = follow_users(:one)
   end
 
-  test "should follow_user" do
+  test "should follow user" do
     assert_difference('FollowUser.count') do
       post :create, FollowedUser: @follow_user.follow_user_id, FollowingUser: @follow_user.user_id
     end
@@ -13,7 +13,7 @@ class FollowUsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should unfollow_user" do
+  test "should unfollow user" do
     assert_difference('FollowUser.count', -1) do
       delete :destroy, UnfollowedUser: @follow_user.follow_user_id, FollwingUser: @follow_user.user_id
     end
